14b4d4870110a840f2b19528fe7f77d3a77b2160
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-04-19 15:12:21 -0300

    Animation on the title banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,10 +1,28 @@
 import { Link } from "react-router-dom";
 import '../../Styles/banner.scss';
+import { useEffect, useState } from "react";
 
 export default function Banner(){
+
+    const [titleBanner, setTitleBanner] = useState('');
+
+    useEffect(()=>{
+        const title = 'The Biggest Store in The World!'
+        let counter = 0;
+        let titleConstructor = '';
+        let timer = setInterval(()=>{
+            if(counter < title.length){
+                titleConstructor = titleConstructor + title[counter++];
+                setTitleBanner(titleConstructor);
+            }else{
+                clearInterval(timer);
+            }
+        }, 200);
+    }, []);
+
     return(
         <div className="banner-container">
-            <h1>The Biggest Store in The World!</h1>
+            <h1>{titleBanner}</h1>
             <h2>Find all you want for a low price!</h2>
             <Link to="/store" className="link">See Store</Link>
         </div>
