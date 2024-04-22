859c4253de2e7757a0ae350c53cb70ba14e9497c
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-04-22 16:03:27 -0300

    start to make a Search component

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,9 +1,17 @@
+import { useState } from 'react';
 import '../../Styles/search.scss';
 
 export default function Search(){
+
+    const [searchValue, setSearchValue] = useState('');
+
+    function handleSearch(event){
+        setSearchValue(event.target.value);
+    }
+
     return(
         <div className="container-search">
-            <input type="text" placeholder="Cade eu?"/>
+            <input type="text" value={searchValue} onChange={handleSearch} placeholder='Find what you want...'/>
         </div>
     );
 }
